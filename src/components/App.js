191ae236d7c1cfc9568c8bca14b02b0a6b7b4596
Richard Odhiambo Otieno <kotienor1@gmail.com>
2024-05-28 09:54:58 +0300

    React-Hooks-Simple-Dta-Fetching-Lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,27 @@
-// create your App component here
+
+import React, { useState, useEffect } from "react";
+
+function App() {
+    const [ imagePic, setImagePic] = useState([]);
+    const [ isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+          .then((response) => response.json())
+          .then((data) => {
+            setImagePic(data.message);
+            setIsLoaded(true);
+
+          });
+    }, []);
+   //shown when the data has not been loaded
+   if (!isLoaded) return <p>Loading...</p>;
+
+  return (
+    <div>
+      <img src={imagePic} alt="A Random Dog" />
+    </div>
+  );
+}
+
+export default App;
